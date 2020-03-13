7d70f860a51443f350756924acc8b1cd243f6e3c
ANIRUDDHA Kulkarni <41265081+Aniruddha25@users.noreply.github.com>
2020-03-13 22:39:54 +0530

    Add files via upload

---
+++ datatype.rs
@@ -0,0 +1,23 @@
+fn main()
+{
+    let a :f32= 23.00;
+    let b = "Aniruddha";
+    let mut c = true;//by using mut keyword we can change value of variable otherwise it  will give error
+    let d ='a';
+    println!("a = {}",a);
+    println!("My name is  : {}",b);
+    println!("c : {}",c);
+    println!("char is  : {}",d);
+    c=false;
+    println!("c : {}",c);
+    //Changing datatype of variable 
+   let  name = "aniruddha";
+   let name=name.len();
+    println!("{}",name);
+    
+
+
+
+
+
+}
